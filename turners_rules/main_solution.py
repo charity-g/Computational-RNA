3084bac25773133a394a15b31ce612416e897b70
cherr <charity.grey1@gmail.com>
2026-01-06 22:15:28 -0800

    pivot turner map to get result

--- turners_rules/main_solution.py
+++ turners_rules/main_solution.py
@@ -4,7 +4,7 @@ import pandas as pd
 pairs = ['AU', 'CG', 'GC', 'UA', 'GU', 'UG'] # GU is wobble
 
 turner_map = pd.read_csv('turner_map.csv')
-turner_map = pd.pivot_table(turner_map, index=pairs, values='energy')
+turner_map = pd.pivot_table(turner_map, index=["top"], columns=["bottom"], values="energy")
 
 turner_loop_map = pd.read_csv('turner_loop_map.csv')
 
